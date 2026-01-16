7cafc96e4bbcf987fb96e30a850175f7329b9604
Mochamad Imam Ghozalie <mimamghozalie@gmail.com>
2026-01-16 18:25:07 +0700

    fix(build): limit protobuf-src to linux target (windows/mac use system protoc)

--- core/build.rs
+++ core/build.rs
@@ -1,5 +1,7 @@
 fn main() -> Result<(), Box<dyn std::error::Error>> {
+    #[cfg(target_os = "linux")]
     std::env::set_var("PROTOC", protobuf_src::protoc());
+    
     tonic_build::compile_protos("proto/node_sync.proto")?;
     Ok(())
 }
